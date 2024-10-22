4953f0b3e62aa57b6093504b07ba594acc932f0c
claudemarjean <marjean.8888@gmail.com>
2024-10-23 01:14:23 +0300

    make entity Article

---
+++ src/Entity/Article.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $article_id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $article_description = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $article_date = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $article_datemodif = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArticleId(): ?int
+    {
+        return $this->article_id;
+    }
+
+    public function setArticleId(int $article_id): static
+    {
+        $this->article_id = $article_id;
+
+        return $this;
+    }
+
+    public function getArticleDescription(): ?string
+    {
+        return $this->article_description;
+    }
+
+    public function setArticleDescription(?string $article_description): static
+    {
+        $this->article_description = $article_description;
+
+        return $this;
+    }
+
+    public function getArticleDate(): ?\DateTimeInterface
+    {
+        return $this->article_date;
+    }
+
+    public function setArticleDate(\DateTimeInterface $article_date): static
+    {
+        $this->article_date = $article_date;
+
+        return $this;
+    }
+
+    public function getArticleDatemodif(): ?\DateTimeInterface
+    {
+        return $this->article_datemodif;
+    }
+
+    public function setArticleDatemodif(?\DateTimeInterface $article_datemodif): static
+    {
+        $this->article_datemodif = $article_datemodif;
+
+        return $this;
+    }
+}
